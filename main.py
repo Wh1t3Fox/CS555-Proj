baedd5bd6d2c5d73c113504c353e5d922cd8ac5f
cawest1221 <cawest1221@gmail.com>
2014-03-23 11:35:26 -0400

    Prevent adding reverse edges

--- main.py
+++ main.py
@@ -2,21 +2,30 @@
 
 from graph_tool.all import *
 
+g = Graph(directed=False)
+
 
 def create_graph():
+    #open the file to read
     with open('matrix.txt', 'r') as fr:
+        #See how many nodes we need
         size =  len(fr.readline().split())
+        #generate the nodes
         g.add_vertex(size)
+        #Seek back to beginning of the file
         fr.seek(0)
+        #iterate each row with the line number
         for line,row in enumerate(fr):
+            #Turn the row into a list/array
                row = row.split()
+               #Iterate over each col in the row
                for pos,item  in enumerate(row):
-                   if item == str(1):
+                  #Create an edge if the item is 1 and don't add reverse edge
+                   if item == str(1) and not g.edge(pos, line):
                        g.add_edge(line, pos)
 
 
 
 if __name__ == '__main__':
-    g = Graph(directed=False)
     create_graph()
     graph_draw(g, vertex_text=g.vertex_index, vertex_font_size=18, output_size=(500, 500), output="nodes.png")
