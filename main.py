c5e1049f398eebd750dbd08d3c7f91b4d25ed6d6
cawest1221 <cawest1221@gmail.com>
2014-03-23 15:03:58 -0400

    Added function for drawing graph

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ Members:
 '''
 from graph_tool.all import *
 
-g = Graph(directed=False)
+g1 = Graph(directed=False)
+g2 = Graph(directed=False)
 
-
-def create_graph(filename):
+def create_graph(filename, graph):
     #open the file to read
     with open(filename, 'r') as fr:
         #See how many nodes we need
         size =  len(fr.readline().split())
         #generate the nodes
-        g.add_vertex(size)
+        graph.add_vertex(size)
         #Seek back to beginning of the file
         fr.seek(0)
         #iterate each row with the line number
@@ -29,11 +29,13 @@ def create_graph(filename):
                #Iterate over each col in the row
                for pos,item  in enumerate(row):
                   #Create an edge if the item is 1 and don't add reverse edge
-                   if item == str(1) and not g.edge(pos, line):
-                       g.add_edge(line, pos)
+                   if item == str(1) and not graph.edge(pos, line):
+                       graph.add_edge(line, pos)
 
-
+def draw_graph(graph):
+    graph_draw(graph, vertex_text=graph.vertex_index, vertex_font_size=18,\
+            output_size=(500, 500), output="nodes.png")
 
 if __name__ == '__main__':
-    create_graph('g1.txt')
-    graph_draw(g, vertex_text=g.vertex_index, vertex_font_size=18, output_size=(500, 500), output="nodes.png")
+    create_graph('g1.txt', g1)
+    draw_graph(g1)
