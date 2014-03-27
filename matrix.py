9885c6bbd8bef8bc0e10ee6b860471a6870ecb28
cawest1221 <cawest1221@gmail.com>
2014-03-26 23:03:05 -0400

    Matrix class creates a matrix from a file

--- matrix.py
+++ matrix.py
@@ -1,11 +1,16 @@
 
 class Matrix:
 
-    def __init__(self, cols, rows):
-        self.cols = cols
-        self.rows = rows
+    def __init__(self, filename):
+        self.matrix = []
+        self.create_from_file(filename)
 
-        self.matrix = [['0' for i in xrange(cols)] for j in xrange(rows)]
+        #self.matrix = [['0' for i in xrange(cols)] for j in xrange(rows)]
+
+    def create_from_file(self, filename):
+        with open(filename, 'r') as fr:
+            for line in fr:
+                self.matrix.append([i for i in line.split()])
 
     def set_item(self, row, col, value):
         self.matrix[row-1][col-1] = str(value)
@@ -14,4 +19,4 @@ class Matrix:
         return self.matrix[row-1][col-1]
 
     def __repr__(self):
-        return '\n'.join([' '.join(self.matrix[i]) for i in range(self.rows)])
+        return '\n'.join([' '.join(self.matrix[i]) for i in range(len(self.matrix))])
