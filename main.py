a936936a1bed56288c8019d52cbbe90f4bdb74aa
cawest1221 <cawest1221@gmail.com>
2014-03-27 15:53:59 -0400

    Implemented the matrix class

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ Members:
     Michael Kouremetis
 '''
 from graph_tool.all import *
+from matrix import Matrix
 
 g1 = Graph(directed=False)
 g2 = Graph(directed=False)
@@ -24,25 +25,13 @@ def create_matrix(filename, graph):
                     fw.write('0 ')
             fw.write('\n')
 
-#Create a graph from a matrix file
-def create_graph(filename, graph):
-    #open the file to read
-    with open(filename, 'r') as fr:
-        #See how many nodes we need
-        size =  len(fr.readline().split())
-        #generate the nodes
-        graph.add_vertex(size)
-        #Seek back to beginning of the file
-        fr.seek(0)
-        #iterate each row with the line number
-        for line,row in enumerate(fr):
-            #Turn the row into a list/array
-               row = row.split()
-               #Iterate over each col in the row
-               for pos,item  in enumerate(row):
-                  #Create an edge if the item is 1 and don't add reverse edge
-                   if item == str(1) and not graph.edge(pos, line):
-                       graph.add_edge(line, pos)
+#Create a graph from a matrix
+def create_graph(matrix, graph):
+    graph.add_vertex(len(list(matrix)))
+    for line, row in enumerate(matrix):
+        for pos, item in enumerate(row):
+            if item == str(1) and not graph.edge(pos, line):
+                graph.add_edge(line, pos)
 
 #Create an image of a graph
 def draw_graph(graph):
@@ -50,5 +39,6 @@ def draw_graph(graph):
             output_size=(500, 500), output="nodes.png")
 
 if __name__ == '__main__':
-    create_graph('g1.txt', g1)
+    g1_matrix = Matrix('g1.txt')
+    create_graph(g1_matrix, g1)
     draw_graph(g1)
