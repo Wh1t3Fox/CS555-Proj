8d8bdaaeb723bf237a09b386b98f58a73e3040f9
cawest1221 <cawest1221@gmail.com>
2014-03-28 13:04:03 -0400

    multithreaded echo server w/ client

--- client.py
+++ client.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
 import socket
+from matrix import Matrix
+from graph_tool.all import *
 
 s = socket.socket()
 host = '127.0.0.1'
 port = 44444
 s.connect((host,port))
 
-print s.recv(1024)
-s.close
+while True:
+    try:
+        data = s.recv(1024)
+        print data    
+    
+        info = raw_input("Text to send: ")
+        s.send(info)
+    except:
+        break
+        s.close
--- server.py
+++ server.py
@@ -3,6 +3,26 @@
 import socket
 from matrix import Matrix
 from random import randint
+import thread
+
+def handler(client, addr):
+    while True:
+        try:
+            data = client.recv(1024)
+            if data.find('QUIT') != -1:
+                break
+            
+            if randint(1,2) == 1:
+                #alpha and the permutation Q
+                pass
+            else:
+                #pi and the subgraph Q'
+                pass
+        
+            msg = 'echoed:...{}'.format(data)
+            client.send(msg)
+        except:
+            client.close()
 
 s = socket.socket()
 host = '127.0.0.1'
@@ -12,15 +32,11 @@ s.bind((host,port))
 s.listen(5)
 
 while True:
-    client, addr = s.accept()
-    print('Connected to {}'.format(addr))
-    client.send('Welcome!')
+    try:
+        client, addr = s.accept()
+        print('Connected to {}'.format(addr))
+        client.send('Welcome!')
+        t = thread.start_new_thread(handler, (client, addr))
+    except:
+        break
 
-    if randint(1,2) == 2:
-        #alpha and the permutation Q
-        pass
-    else:
-        #pi and the subgraph Q'
-        pass
-
-    client.close()
