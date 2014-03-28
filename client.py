29c8335e07cc8d8b413a07ff2a25a7e09dbfcf84
cawest1221 <cawest1221@gmail.com>
2014-03-28 18:15:30 -0400

    Client and server fake interactive 100 rounds

--- client.py
+++ client.py
@@ -5,20 +5,40 @@ import pickle
 from matrix import Matrix
 from graph_tool.all import *
 
+
 s = socket.socket()
 host = '127.0.0.1'
 port = 44444
-s.connect((host,port))
+alpha = None
+pi = None
+num_rounds = 0
 
-m = Matrix('g2.txt')
-txt = pickle.dumps(m)
-while True:
-    try:
-        data = s.recv(1024)
-        print data    
-        
-        info = raw_input("Enter to continue: ")
-        s.send(txt)
-    except:
-        break
-        s.close
+
+if __name__ == '__main__':
+    s.connect((host,port))
+
+    m = Matrix('g2.txt')
+    info = ['q', m]
+    txt = pickle.dumps(info)
+    s.send(txt)
+    while True:
+        try:
+            if num_rounds == 100:
+                break
+                
+            data = s.recv(1024)
+            print data    
+            
+            if data.find('alpha and Graph Q') != -1:
+                info = [1, 'a', 'q'] 
+                
+            elif data.find('pi and the subgraph') != -1:
+                info = [2, 'pi', 'subgraph']
+                
+            msg = pickle.dumps(info)            
+            s.send(msg)
+            
+            num_rounds += 1
+        except:
+            break
+            s.close
--- server.py
+++ server.py
@@ -8,12 +8,31 @@ import thread
 
 g1 = Matrix('g1.txt')
 g2 = Matrix('g2.txt')
+q = None
+
+s = socket.socket()
+host = '127.0.0.1'
+port = 44444
 
 def handler(client, addr):
     while True:
         try:
             data = client.recv(1024)
-            matrix = pickle.loads(data)
+            if not data:
+                break
+               
+            lst = pickle.loads(data)
+            
+            if lst[0] == 'q':
+                global q
+                q = lst[1]
+                
+            elif lst[0] == 1:
+                print lst
+             
+            elif lst[0] == 2:
+                print lst
+                
             if randint(1,2) == 1:
                 #alpha and the permutation Q
                 msg = 'Please send alpha and Graph Q'
@@ -24,19 +43,16 @@ def handler(client, addr):
         except:
             client.close()
 
-s = socket.socket()
-host = '127.0.0.1'
-port = 44444
-s.bind((host,port))
+if __name__ == '__main__':
+    s.bind((host,port))
+    s.listen(5)
 
-s.listen(5)
-
-while True:
-    try:
-        client, addr = s.accept()
-        print('Connected to {}'.format(addr))
-        client.send('Welcome!')
-        t = thread.start_new_thread(handler, (client, addr))
-    except:
-        break
+    while True:
+        try:
+            client, addr = s.accept()
+            print('Connected to {}'.format(addr))
+            client.send('Please Login!')
+            t = thread.start_new_thread(handler, (client, addr))
+        except:
+            break
 
