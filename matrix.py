4f15a91ffd523dfa5d7ffeb9a05e3d8741188559
cawest1221 <cawest1221@gmail.com>
2014-03-28 12:34:03 -0400

    Added some comments

--- matrix.py
+++ matrix.py
@@ -5,8 +5,6 @@ class Matrix:
         self.matrix = []
         self.create_from_file(filename)
 
-        #self.matrix = [['0' for i in xrange(cols)] for j in xrange(rows)]
-        
     def __getitem__(self, row):
         return self.matrix[row]
     
@@ -18,15 +16,18 @@ class Matrix:
 
     def __repr__(self):
         return '\n'.join([' '.join(self.matrix[i]) for i in range(len(self.matrix))])
-              
+    
+    #Adjacency matrix file
     def create_from_file(self, filename):
         with open(filename, 'r') as fr:
             for line in fr:
                 self.matrix.append([i for i in line.split()])
-
+    
+    #col is a number 0-(N-1)
     def get_col(self, col):
         return [i[col] for i in self.matrix]
-
+    
+    #Values is a list of strings
     def set_col(self, col, values):
         for v,m in zip(values, self.matrix):
             m[col] = v
