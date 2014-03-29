eaeb40f83c1fd0591dc226da6050fcd82e552adb
cawest1221 <cawest1221@gmail.com>
2014-03-29 19:19:06 -0400

    Added function to create random permutations

--- client.py
+++ client.py
@@ -13,6 +13,43 @@ alpha = None
 pi = None
 num_rounds = 0
 
+#Create a permutation matrix file
+def create_permutation(filename, size):
+    s = set()
+    tmp = []
+    while len(set(tmp)) != size:
+        tmp.append(randint(0, size-1))
+    order = [x for x in tmp if x not in s and not s.add(x)]
+    with open(filename, 'w') as fw:
+        for  i in range(size):
+            for j in range(size):
+                if order[i] == j:
+                    fw.write('1 ')
+                else:
+                    fw.write('0 ')
+            fw.write('\n')
+
+#Create a matrix file from a graph
+def create_matrix(filename, graph):
+    with open(filename, 'w') as fw:
+        for i in range(graph.num_vertices()):
+            for j in range(graph.num_vertices()):
+                if graph.edge(i, j):
+                    fw.write('1 ')
+                else:
+                    fw.write('0 ')
+            fw.write('\n')
+
+#Create a graph from a matrix
+def create_graph(matrix, graph):
+    graph.add_vertex(len(matrix))
+    for line, row in enumerate(matrix):
+        for pos, item in enumerate(row):
+            if item == str(1) and not graph.edge(pos, line):
+                graph.add_edge(line, pos)
+                
+                
+                
 
 if __name__ == '__main__':
     s.connect((host,port))
