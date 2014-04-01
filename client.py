3eadf8d3f99e7c74dde0be0e23bf99b47ab002a2
Craig West <cawest1221@gmail.com>
2014-04-01 14:09:57 -0400

    Now can create a permutation matrix from the Matrix class

--- client.py
+++ client.py
@@ -13,28 +13,11 @@ import sys
 import socket
 import pickle
 from matrix import Matrix
-from random import randint
 from copy import deepcopy
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '127.0.0.1'
 port = 44444
-
-#Create a permutation matrix file
-def create_permutation(filename, size):
-    s = set()
-    tmp = []
-    while len(set(tmp)) != size:
-        tmp.append(randint(0, size-1))
-    order = [x for x in tmp if x not in s and not s.add(x)]
-    with open(filename, 'w') as fw:
-        for i in range(size):
-            for j in range(size):
-                if order[i] == j:
-                    fw.write('1 ')
-                else:
-                    fw.write('0 ')
-            fw.write('\n')
 
 #DFS on an adjacency matrix
 def dfs(matrix, start_node):
@@ -47,7 +30,7 @@ def dfs(matrix, start_node):
             try:
                 graph[line]
                 if value == '1':
-                    graph[line].append(col)                
+                    graph[line].append(col)
             except:
                 graph[line] = []
 
@@ -70,8 +53,7 @@ if __name__ == '__main__':
 
     g1 = Matrix('g1.txt')
     g2 = Matrix('g2.txt')
-    create_permutation('alpha.txt', len(g2))
-    alpha = Matrix('alpha.txt')
+    alpha = Matrix(len(g2))
     q = deepcopy(g2)
     q.permute(alpha)
     #Need to commit to Q here and create subgraph q'
--- matrix.py
+++ matrix.py
@@ -7,29 +7,34 @@ Members:
     David Hersh
     Michael Kouremetis
 '''
+from random import randint
+import os
 
 class Matrix:
 
     """
     Initalize the matrix
     """
-    def __init__(self, filename):
+    def __init__(self, filename=None):
         self.matrix = []
-        self.create_from_file(filename)
+        if isinstance(filename, int):
+            self.create_permutation(filename)
+        elif os.path.isfile(filename):
+            self.create_from_file(filename)
 
     """
     Returns a given row
     """
     def __getitem__(self, row):
         return self.matrix[row]
-    
+
     """
     Define how we set a rows value.
     The value must be a list of strings
     """
     def __setitem__(self, row, values):
         self.matrix[row] = values
-        
+
     """
     Return the size of the matrix in
     the number of rows
@@ -42,7 +47,7 @@ class Matrix:
     """
     def __repr__(self):
         return '\n'.join([' '.join(self.matrix[i]) for i in range(len(self.matrix))])
-    
+
     """
     Creates a matrix from an adjacency matrix file
     """
@@ -50,14 +55,14 @@ class Matrix:
         with open(filename, 'r') as fr:
             for line in fr:
                 self.matrix.append([i for i in line.split()])
-    
+
     """
     Return the values of a given col.
     The input must be a number [0, N-1]
     """
     def get_col(self, col):
         return [i[col] for i in self.matrix]
-    
+
     """
     Set the values of a given col.
     The input must be a list of strings
@@ -65,19 +70,19 @@ class Matrix:
     def set_col(self, col, values):
         for v,m in zip(values, self.matrix):
             m[col] = v
-            
+
     """
     Rotate the current matrix 90 degrees CW
     """
     def rotate_cw(self):
         self.matrix = [self.get_col(i)[::-1] for i in range(len(self.matrix))]
-        
+
     """
     Rotate the current matrix 90 degrees CCW
     """
     def rotate_ccw(self):
         self.matrix = [self.get_col(i) for i in range(len(self.matrix)-1, -1, -1)]
-        
+
     """
     Rotate the current matrix 180 degrees
     """
@@ -92,7 +97,7 @@ class Matrix:
         order = [pos for row in matrix for pos,item in enumerate(row) if item == str(1)]
         tmp = self.matrix
         self.matrix = [tmp[i] for i in order]
-    
+
     """
     Create matrix file that is the permutation
     between the current matrix and a given matrix
@@ -117,3 +122,21 @@ class Matrix:
             if self.matrix[i] != matrix[i]:
                 return False
         return True
+
+    """
+    Create a permutation matrix
+    """
+    def create_permutation(self, size):
+        s = set()
+        t = []
+        while len(set(t)) != size:
+            t.append(randint(0, size-1))
+        order = [x for x in t if x not in s and not s.add(x)]
+        for i in range(size):
+            r = []
+            for j in range(size):
+                if order[i] == j:
+                    r.append('1')
+                else:
+                    r.append('0')
+            self.matrix.append(r)
