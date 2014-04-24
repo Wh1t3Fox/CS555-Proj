14ab766c6c0b653f2be6371f676b0044beda0818
David <davidhersh0402@gmail.com>
2014-04-24 00:37:38 -0400

    Isomorphsim function
    I added the isomorphism functionality to the matrix class. It returns a
    dictionary containing the isomorphism function, and a new Matrix object
    of the isomorphism adjacency matrix.

--- matrix.py
+++ matrix.py
@@ -10,6 +10,7 @@ Members:
 from random import randint
 import os
 import hashlib
+from copy import deepcopy #this is only here for testing
 
 class Matrix:
 
@@ -22,6 +23,8 @@ class Matrix:
             self.create_permutation(filename)
         elif os.path.isfile(filename):
             self.create_from_file(filename)
+        elif filename is 'empty':
+            self.create_empty_matrix(size)
         else: #this method of calling create_matrix is ugly, but works
             self.create_matrix(size)
 
@@ -52,11 +55,12 @@ class Matrix:
         return '\n'.join([''.join(str(self.matrix[i])) for i in range(len(self.matrix))])
 
     """
-    Creates a matrix
+    Creates a matrix, and adds random values
     """
     def create_matrix(self, size):
-        for i in range(size):
-            row = [0 for y in range(size)]
+        #initialize matrix to 0
+        for i in xrange(size):
+            row = [0 for y in xrange(size)]
             self.matrix.append(row)
         for index, i in enumerate(self.matrix):
             for index2, j in enumerate(i):
@@ -65,6 +69,12 @@ class Matrix:
                     if r is 1:
                         self.matrix[index][index2] = r
                         self.matrix[index2][index] = r
+
+    def create_empty_matrix(self, size):
+        for i in xrange(size):
+            row = [0 for y in xrange(size)]
+            self.matrix.append(row)
+            
     """
     Creates a supergraph
     """
@@ -79,35 +89,79 @@ class Matrix:
         newrowlength = len(self.matrix[0]) + top + bottom
         for i in self.matrix: #add elements to old graph rows to make them
                               #new size rows, and initialize to 0
-            for x in range(top):
+            for x in xrange(top):
                 i.insert(x, 0)
-            for y in range(bottom):
+            for y in xrange(bottom):
                 i.append(0)
         #initialize and add top and bottom rows
-        for x in range(top):
-            row = [0 for y in range(newrowlength)]
+        for x in xrange(top):
+            row = [0 for y in xrange(newrowlength)]
             self.matrix.insert(x, row)
-        for x in range(bottom):
-            row = [0 for y in range(newrowlength)]
+        for x in xrange(bottom):
+            row = [0 for y in xrange(newrowlength)]
             self.matrix.append(row)
         
         #set vertex connections (i.e. ones) randomly in added rows,
         #and in newly added elements in old rows
-        for x in range(top):
-            for y in range(newrowlength):
+        for x in xrange(top):
+            for y in xrange(newrowlength):
                 r = randint(0, 1)
                 if r is 1:
                     self.matrix[x][y] = r
                     self.matrix[y][x] = r
-        for x in range(bottom):
-            for y in range(newrowlength):
+        for x in xrange(bottom):
+            for y in xrange(newrowlength):
                 if self.matrix[newrowlength-x-1][y] is not 1:
                     r = randint(0, 1)
                     if r is 1:
                         self.matrix[newrowlength-x-1][y] = r
                         self.matrix[y][newrowlength-x-1] = r
         
-            
+    """
+    Creates an isomorphic graph, returns the isomorphism function in
+    dictionary form and the isomorphism graph as a new Matrix object
+    """
+    def isomorphism(self):
+        orig_graph = {}
+        isofunction = {}
+        new_graph = {}
+        temp = range(len(self.matrix))
+        #fill orig_graph dictionary with nodes as keys and
+        #lists of the nodes they are connected to as the values
+        for index, i in enumerate(self.matrix):
+            x = []
+            for index2, j in enumerate(i):
+                if j is 1:
+                    x.append(index2)
+            orig_graph[index] = x
+        #create isomorphism function by pairing each node
+        #with a random node. these pairs are put in the dictionary
+        #isofunction
+        for i in xrange(len(self.matrix)):
+            r = randint(0, len(temp)-1)
+            isofunction[i] = temp.pop(r)
+        #modify orig_graph values (NOT keys) by applying
+        #the isomorphism
+        for key, value in orig_graph.iteritems():
+            for index, i in enumerate(value):
+                for key2 in isofunction.iterkeys():
+                    if i is key2:
+                        orig_graph[key][index] = isofunction[key2]
+                        break
+        #Apply the isomorphism to the keys in orig_graph,
+        #storing the resulting full isomorphism in new_graph
+        for key in orig_graph.iterkeys():
+            for key2 in isofunction.iterkeys():
+                if key is key2:
+                    new_graph[isofunction[key2]] = orig_graph[key]
+                    break
+        #Turn new_graph dictionary into adjacency matrix, and
+        #return the new matrix
+        new_matrix = Matrix('empty', len(self.matrix))
+        for key, value in new_graph.iteritems():
+            for i in value:
+                new_matrix[key][i]=1
+        return isofunction, new_matrix
     
     """
     Creates a matrix from an adjacency matrix file
