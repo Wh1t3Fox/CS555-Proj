bac023455bd3a9c347b92c0c87a913fe2c7d8a6d
Craig West <cawest1221@gmail.com>
2014-04-24 20:20:18 -0400

    Started to add optional arguments

--- client.py
+++ client.py
@@ -19,10 +19,10 @@ from commitment import *
 from copy import deepcopy
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-g1','--graph1', help='Name of the adjacency matrix file for G1', required=False)
-parser.add_argument('-g2','--graph2', help='Name of the adjacency matrix file for G2', required=False)
-parser.add_argument('-s','--subgraph', help='Name of the adjacency matrix file for the subgraph', required=False)
-parser.add_argument('-i','--isomorphism', help='Name of the adjacency matrix file for the isomorphism', required=False)
+parser.add_argument('-g1','--graph1', type=file, help='Name of the adjacency matrix file for G1', required=False)
+parser.add_argument('-g2','--graph2', type=file, help='Name of the adjacency matrix file for G2', required=False)
+parser.add_argument('-s','--subgraph', type=file, help='Name of the adjacency matrix file for the subgraph', required=False)
+parser.add_argument('-i','--isomorphism', type=file, help='Name of the adjacency matrix file for the isomorphism', required=False)
 args = parser.parse_args()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
